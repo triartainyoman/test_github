f98741815e5b82dd364bebb711c92e684ad03a97
triartainyoman <triarta.art@gmail.com>
2021-03-12 10:17:14 +0800

    test commit

---
+++ android/app/src/main/kotlin/com/example/parsing_data/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parsing_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Parsing Data',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Parsing Data"),
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ReusableCard(icon: Icons.home, title: "Home",),
+            ReusableCard(icon: Icons.share, title: "Share",),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({this.icon, this.title});
+
+  final IconData icon;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Icon(icon),
+          Text(title),
+        ],
+      ),
+   );
+  }
+}
